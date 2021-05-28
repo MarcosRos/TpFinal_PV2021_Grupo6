d7c75e7c26d5e951e95d14187606ef85c972460d
marco <marco@DESKTOP-A9QMSHG>
2021-05-28 15:16:50 -0300

    Inicializaciond el proyecto

---
+++ src/main/java/ar/edu/unju/edm/TpFinalPv2021Grupo6Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalPv2021Grupo6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalPv2021Grupo6Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unju/edm/TpFinalPv2021Grupo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpFinalPv2021Grupo6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
