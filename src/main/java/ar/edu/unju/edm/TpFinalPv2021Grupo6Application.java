a6410f41e2c4181e77828a10d5d74eaaa8c42085
LucasMesconi <LucasMesconi@LAPTOP-GDTTGJ9M>
2021-05-28 15:47:25 -0300

    mensaje de buenas tardes

--- src/main/java/ar/edu/unju/edm/TpFinalPv2021Grupo6Application.java
+++ src/main/java/ar/edu/unju/edm/TpFinalPv2021Grupo6Application.java
@@ -8,6 +8,8 @@ public class TpFinalPv2021Grupo6Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpFinalPv2021Grupo6Application.class, args);
+		
+		System.out.println("Buenas Tardes");
 	}
 
 }
