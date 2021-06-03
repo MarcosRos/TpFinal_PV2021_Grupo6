ab4c4708d75cd2bb96558b5391ae60b597277c55
LucasMesconi <LucasMesconi@LAPTOP-GDTTGJ9M>
2021-06-03 20:58:26 -0300

    a

--- src/main/java/ar/edu/unju/edm/controller/MainController.java
+++ src/main/java/ar/edu/unju/edm/controller/MainController.java
@@ -1,20 +1,19 @@
 package ar.edu.unju.edm.controller;
 
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.jboss.jandex.Main;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-//import ar.edu.unju.edm.model.Home;
-
 @Controller
 public class MainController {
 	
-	/*@Autowired
-	Home unHome;*/
+	@Autowired
+    Main unMain;
 
-	/*@GetMapping({ "/home"})
-	public String cargarHome(Model model) {
-		return "home";
-	}*/
+    @GetMapping({ "/", "/login", "/home", "/index", "/login?error=true" })
+    public String cargarHome(Model model) {
+        return "home";
+    }
 }
